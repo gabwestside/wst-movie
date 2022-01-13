ebcdbf70a5ea0c21dafbbe37e6f408a1dac3108e
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-01-13 00:03:57 -0300

    Form layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,7 +1,45 @@
+import './styles.css';
+
 function Form() {
-    return (
-        <h1>Form Page</h1>
-    );
+  const movie = {
+    id: 1,
+    image:
+      "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+    title: "The Witcher",
+    count: 2,
+    score: 4.5,
+  };
+
+  return (
+    <div className="wstmovie-form-container">
+      <img className="wstmovie-movie-card-image" src={movie.image} alt={movie.title} />
+      <div className="wstmovie-card-bottom-container">
+        <h3>{movie.title}</h3>
+        <form className="wstmovie-form">
+          <div className="form-group dsmovie-form-group">
+            <label htmlFor="email">Informe seu email</label>
+            <input type="email" className="form-control" id="email" />
+          </div>
+          <div className="form-group wstmovie-form-group">
+            <label htmlFor="score">Informe sua avaliação</label>
+            <select className="form-control" id="score">
+              <option>1</option>
+              <option>2</option>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+            </select>
+          </div>
+          <div className="wstmovie-form-btn-container">
+            <button type="submit" className="btn btn-primary wstmovie-btn">
+              Salvar
+            </button>
+          </div>
+        </form>
+        <button className="btn btn-primary wstmovie-btn mt-3">Cancelar</button>
+      </div>
+    </div>
+  );
 }
 
 export default Form;
