7b62f859154e60b9ff037bc532f5995820a2c2dd
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-01-12 21:43:38 -0300

    Adicionando o backend

---
+++ backend/src/main/java/com/westside/westsidemovie/WestsidemovieApplication.java
@@ -0,0 +1,13 @@
+package com.westside.westsidemovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WestsidemovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WestsidemovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/westside/westsidemovie/WestsidemovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.westside.westsidemovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WestsidemovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
