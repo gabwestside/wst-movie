7114b1ee377a1d82891b6d0f1313a6e71fc90d35
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-01-15 19:05:05 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/westside/westsidemovie/entities/Score.java
@@ -0,0 +1,46 @@
+package com.westside.westsidemovie.entities;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+
+public class Score {
+	@EmbeddedId
+	
+	private ScorePK id = new ScorePK();
+	private Double value;
+	
+	public Score() {
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void serUser(User user) {
+		id.setUser(user);
+	}
+	
+	public Score(Double value) {
+		this.value = value;
+	}
+
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+}
