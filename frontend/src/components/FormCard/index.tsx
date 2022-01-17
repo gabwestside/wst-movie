fa98c751fb27b6de43211ffd5bd1a2d8c50e2d61
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-01-17 00:58:52 -0300

    Save, useNavigate

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Movie } from "types/movie";
+import { validateEmail } from "utils/validate";
 import { BASE_URL } from "utils/requests";
 import "./styles.css";
 
@@ -10,14 +11,41 @@ type Props = {
 };
 
 function FormCard({ movieId }: Props) {
+  const navigate = useNavigate();
+
   const [movie, setMovie] = useState<Movie>();
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/movies/${movieId}`)
-        .then((response) => {
-            setMovie(response.data);
-        });
+    axios.get(`${BASE_URL}/movies/${movieId}`).then((response) => {
+      setMovie(response.data);
+    });
   }, [movieId]);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const email = (event.target as any).email.value;
+    const score = (event.target as any).score.value;
+
+    if (!validateEmail(email)) {
+      return;
+    }
+
+    const config: AxiosRequestConfig = {
+      baseURL: BASE_URL,
+      method: "PUT",
+      url: "/scores",
+      data: {
+        email: email,
+        movieId: movieId,
+        score: score,
+      },
+    };
+
+    axios(config).then((response) => {
+      navigate("/");
+    });
+  };
 
   return (
     <div className="wstmovie-form-container">
@@ -28,7 +56,7 @@ function FormCard({ movieId }: Props) {
       />
       <div className="wstmovie-card-bottom-container">
         <h3>{movie?.title}</h3>
-        <form className="wstmovie-form">
+        <form className="wstmovie-form" onSubmit={handleSubmit}>
           <div className="form-group dsmovie-form-group">
             <label htmlFor="email">Informe seu email</label>
             <input type="email" className="form-control" id="email" />
