f1360e814bf03b75771160649d6ff6e44b25738c
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-01-16 22:50:05 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,11 +1,18 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
+import { BASE_URL } from "utils/requests";
 import Pagination from "../../components/Pagination";
 
 function Listing() {
+  axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then((response) => {
+      console.log(response.data);
+    });
+
   return (
     <>
       <Pagination />
-      
+
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-x1-3 mb-3">
@@ -31,7 +38,6 @@ function Listing() {
           </div>
         </div>
       </div>
-
     </>
   );
 }
