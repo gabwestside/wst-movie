df871d8f8d5250db9d7100f15c4f42db2f524cb7
Gabriel Rodrigues <gabrielnfl13@gmail.com>
2022-01-17 00:35:08 -0300

    Pagination

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,16 +1,30 @@
+import { MoviePage } from "types/movie";
 import { ReactComponent as Arrow } from "../../assets/arrow.svg";
-import './styles.css';
+import "./styles.css";
 
-function Pagination() {
+type Props = {
+  page: MoviePage;
+  onChange: Function;
+};
+
+function Pagination({ page, onChange }: Props) {
   return (
     <div className="wstmovie-pagination-container">
       <div className="wstmovie-pagination-box">
-        <button className="wstmovie-pagination-button" disabled={true}>
-          <Arrow />
-        </button>
-        <p>{`${1} de ${3}`}</p>
-        <button className="wstmovie-pagination-button" disabled={false}>
+        <button
+          className="wstmovie-pagination-button"
+          disabled={page.first}
+          onClick={() => onChange(page.number - 1)}
+        >
           <Arrow className="wstmovie-flip-horizontal" />
+        </button>
+        <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+        <button
+          className="wstmovie-pagination-button"
+          disabled={page.last}
+          onClick={() => onChange(page.number + 1)}
+        >
+          <Arrow />
         </button>
       </div>
     </div>
